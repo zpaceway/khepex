b52945efe256f6972479e4691266f995e9a6f8b5
Alexandro Tapia <zpaceway@gmail.com>
2023-03-19 06:31:27 -0500

    favico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { SessionProvider } from "next-auth/react";
 
 import { api } from "@/utils/api";
 import LoadingScreen from "@/components/LoadingScreen";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -14,6 +15,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <LoadingScreen />
+      <Head>
+        <link rel="shortcut icon" href="/logo.svg" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
